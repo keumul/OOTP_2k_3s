7f9d336bc4ddbd4bd088eee36f2fc918ea82825a
Kate <82730879+keumul@users.noreply.github.com>
2021-10-04 23:35:53 +0300

    Add files via upload

---
+++ Lab4/MathOperation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    static class MathOperation
+    {
+        public static string Max(MyList list)
+        {
+            string str = list.GetItemByIndex(0);
+            foreach (string item in list.GetList())
+            {
+                if (str.Length < item.Length)
+                {
+                    str = item;
+                }
+            }
+            return str;
+        }
+
+        public static string Min(MyList list)
+        {
+            string str = list.GetItemByIndex(0);
+            foreach (string item in list.GetList())
+            {
+                if (str.Length > item.Length)
+                {
+                    str = item;
+                }
+            }
+            return str;
+        }
+
+        public static int CountOfWords(this string str)
+        {
+            int count = 0;
+            string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            count += words.Length;
+            return count;
+        }
+
+        public static bool IsContainsEmptyElement(this MyList list)
+        {
+            foreach (string item in list.GetList())
+            {
+                if (item == "")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Lab4/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Lab4
+{
+    public class Owner
+    {
+        private readonly int id;
+        private readonly string fio;
+
+        public Owner(int id, string fio)
+        {
+            this.id = id;
+            this.fio = fio;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Owner – ID: {id}, FIO: {fio}.");
+        }
+    }
+}
